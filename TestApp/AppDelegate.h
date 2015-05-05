390e29061ebfa2f9497db88d55d3a67c3ff092ec
Ranjeeta <ranjeeta@Abhinavs-Mac-mini-2.local>
2015-05-05 10:42:26 +0530

    Initial Commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Ranjeeta on 05/05/15.
+//  Copyright (c) 2015 Ranjeeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Ranjeeta on 05/05/15.
+//  Copyright (c) 2015 Ranjeeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
